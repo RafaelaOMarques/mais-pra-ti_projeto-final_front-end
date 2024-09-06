cd0c44fee540fcb4377dfc5e4d0f275d29a8559f
João <paivaseven@gmail.com>
2024-09-06 12:29:03 -0300

    ajustado o css do header para padrao web do figma

--- frontend/frontend-projeto-final/src/components/Header/Header.jsx
+++ frontend/frontend-projeto-final/src/components/Header/Header.jsx
@@ -2,6 +2,10 @@ import { useContext } from "react";
 import { FaSun, FaMoon, FaUserCircle } from "react-icons/fa";
 import { DarkModeContext } from "../../context/DarkModeContext/DarkModeContext";
 import "./Header.css";
+import Logo from "../../assets/globe_1.svg";
+import SearchIcon from "../../assets/search_icon.svg";
+import LoginIcon from "../../assets/login_icon.svg";
+// import DarkThemeIcon from "../../assets/dark_theme_icon.svg";
 
 const Header = () => {
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
@@ -10,34 +14,27 @@ const Header = () => {
     <header className="header-container">
       <div className="logo">
         <img
-          src="https://raw.githubusercontent.com/adarshaacharya/ApiHub/main/assets/logo.png"
+          src={Logo}
           alt="Logo"
         />
         <span>API HUB BRASIL</span>
       </div>
-      <div>
-        <div className="theme-login-container">
-          <button className="theme-switcher" onClick={toggleDarkMode}>
-            {isDarkMode ? <FaSun /> : <FaMoon />}
-          </button>
-          <button className="login-button">
-            <FaUserCircle style={{ marginRight: "5px" }} />
-            Entrar
-          </button>
-        </div>
+      <div className="theme-login-container">
         <div className="search-container">
           <input
-            className="search-input"
             type="text"
-            placeholder="O que você procura?"
           />
-          <button className="search-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path d="M10 2a8 8 0 105.29 14.71l5 5a1 1 0 001.42-1.42l-5-5A8 8 0 0010 2zm0 2a6 6 0 110 12 6 6 0 010-12z" />
-            </svg>
-          </button>
+          <img src={SearchIcon} alt="search-icon"/>
         </div>
+        <button className="login-button">
+          <img src={LoginIcon} alt="login icon" />
+          <span>Entrar</span>
+        </button>
+        <button className="theme-switcher" onClick={toggleDarkMode}>
+          {isDarkMode ? <FaSun /> : <FaMoon />}
+        </button>
       </div>
+
     </header>
   );
 };
