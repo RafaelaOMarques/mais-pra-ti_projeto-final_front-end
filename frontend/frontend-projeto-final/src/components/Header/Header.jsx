e0d76c71e444fd45c42b80088915a3c8c4b6838e
rodrigo <rodrigosv91@gmail.com>
2024-09-23 22:45:01 -0300

    add close icon

--- frontend/frontend-projeto-final/src/components/Header/Header.jsx
+++ frontend/frontend-projeto-final/src/components/Header/Header.jsx
@@ -1,23 +1,32 @@
-import { useContext } from "react";
-import { FaSun, FaMoon, FaUserCircle } from "react-icons/fa";
+import { useContext, useState } from "react";
+import { FaSun, FaMoon, FaTimes } from "react-icons/fa";
 import { DarkModeContext } from "../../context/DarkModeContext/DarkModeContext";
 import "./Header.css";
 import Logo from "../../assets/globe_1.png";
 import Logo2 from "../../assets/globe_2.png";
 import SearchIcon from "../../assets/search_icon.svg";
 import LoginIcon from "../../assets/login_icon.svg";
-// import DarkThemeIcon from "../../assets/dark_theme_icon.svg";
 
 const Header = () => {
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
 
   const Acessar = () => {
-    if (localStorage.getItem("autenticado") == "false") {
+    if (localStorage.getItem("autenticado") === "false") {
       window.location.href = "/Acesso";
     } else {
       localStorage.removeItem("autenticado");
       window.location.href = "/rotaAutenticada";
     }
+  };
+
+  const closeSearch = () => {
+    setIsSearchExpanded(false);
+    setSearchValue("");
+  };
+
+  const toggleSearch = () => {
+    setIsSearchExpanded(true);
   };
 
   return (
@@ -33,14 +42,23 @@ const Header = () => {
         </a>
       </div>
       <div className="theme-login-container">
-        <div className="search-container">
-          <input type="text" />
+        <div
+          className={`search-container ${isSearchExpanded ? "expanded" : ""}`}
+        >
+          <input type="text" onFocus={toggleSearch} />
           <img src={SearchIcon} alt="search-icon" />
+          {isSearchExpanded && (
+            <span className="clear-icon" onClick={closeSearch}>
+              <FaTimes
+                className={`close-icon ${isDarkMode ? "" : "dark-mode"}`}
+              />
+            </span>
+          )}
         </div>
         <button onClick={Acessar} className="login-button">
           <img src={LoginIcon} alt="login icon" />
           <span>
-            {localStorage.getItem("autenticado") == "true" ? "Sair" : "Entrar"}
+            {localStorage.getItem("autenticado") === "true" ? "Sair" : "Entrar"}
           </span>
         </button>
         <button className="theme-switcher" onClick={toggleDarkMode}>
